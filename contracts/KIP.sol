4ca5a611bec922cda38c623a27c7b5c6f4f7fd72
liaoyuan <bizliaoyuan@gmail.com>
2024-12-03 01:58:23 +0800

    change token name

--- contracts/KIP.sol
+++ contracts/KIP.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.22;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract KIP is ERC20 {
-    constructor(address beneficiary) ERC20("KIP", "KIP") {
+    constructor(address beneficiary) ERC20("KIP Protocol", "KIP") {
         _mint(beneficiary, 10000000000 * 10 ** decimals());
     }
 }
